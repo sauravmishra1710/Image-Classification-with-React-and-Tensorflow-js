861046ed0615a8b3dbfcd9162c3ca573d16b46e1
Saurav Mishra <saurav.mishra@live.com>
2023-09-09 21:04:42 +0530

    adding mobile net functionality

--- src/App.js
+++ src/App.js
@@ -1,29 +1,89 @@
 import './App.css';
+import { useState, useEffect, useRef } from 'react';
+import * as mobilenet from "@tensorflow-models/mobilenet";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+    const [isModelLoading, setIsModelLoading] = useState(false)
+    const [model, setModel] = useState(null)
+    const [imageURL, setImageURL] = useState(null);
+    const [results, setResults] = useState([])
+    const [history, setHistory] = useState([])
+
+    const imageRef = useRef()
+    const fileInputRef = useRef()
+
+    const loadModel = async () => {
+        setIsModelLoading(true)
+        try {
+            const model = await mobilenet.load()
+            setModel(model)
+            setIsModelLoading(false)
+        } catch (error) {
+            console.log(error)
+            setIsModelLoading(false)
+        }
+    }
+
+    const uploadImage = (e) => {
+        const { files } = e.target
+        if (files.length > 0) {
+            const url = URL.createObjectURL(files[0])
+            setImageURL(url)
+        } else {
+            setImageURL(null)
+        }
+    }
+
+    const identify = async () => {
+        const results = await model.classify(imageRef.current)
+        setResults(results)
+    }
+
+    const triggerUpload = () => {
+        fileInputRef.current.click()
+    }
+
+    useEffect(() => {
+        loadModel()
+    }, [])
+
+    useEffect(() => {
+        if (imageURL) {
+            setHistory([imageURL, ...history])
+        }
+    }, [imageURL])
+
+    if (isModelLoading) {
+        return <h2>Model Loading...</h2>
+    }
+
+    return (
+        <div className="App">
+            <h1 className='header'>Image Identification</h1>
+            <div className='inputHolder'>
+                <input type='file' accept='image/*' capture='camera' className='uploadInput' onChange={uploadImage} ref={fileInputRef} />
+                <button className='uploadImage' onClick={triggerUpload}>Upload Image</button>
+            </div>
+            <div className="mainWrapper">
+                <div className="mainContent">
+                    <div className="imageHolder">
+                        {imageURL && <img src={imageURL} alt="Upload Preview" crossOrigin="anonymous" ref={imageRef} />}
+                    </div>
+                    {results.length > 0 && <div className='resultsHolder'>
+                        {results.map((result, index) => {
+                            return (
+                                <div className='result' key={result.className}>
+                                    <span className='name'>{result.className}</span>
+                                    <span className='confidence'>Confidence level: {(result.probability * 100).toFixed(2)}% {index === 0 && <span className='bestGuess'>Best Guess</span>}</span>
+                                </div>
+                            )
+                        })}
+                    </div>}
+                </div>
+                {imageURL && <button className='button' onClick={identify}>Identify Image</button>}
+            </div>
+        </div>
+    );
 }
 
 export default App;
