1f0cc2664d2968ee2c39a7d9ffcde4eeef2461a0
Saurav Mishra <saurav.mishra@live.com>
2023-09-11 19:24:55 +0530

    add code comments and correct indentations

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { useState, useEffect, useRef } from 'react';
-import * as mobilenet from "@tensorflow-models/mobilenet";
+import * as mobilenet from "@tensorflow-models/mobilenet"; // Refer: https://github.com/tensorflow/tfjs-models/tree/master/mobilenet
 import CircularProgress from "@mui/material/CircularProgress"; // refer: https://mui.com/material-ui/react-progress/#circular-indeterminate
 import Box from "@mui/material/Box";
 import IconSpinner from 'terra-icon/lib/icon/IconSpinner';
@@ -31,20 +31,27 @@ function App() {
       const divider = document.getElementsByClassName('divider')[0];
       if (!divider) return;
 
-        if (showDivider) divider.style.display = 'block';
-        else divider.style.display = 'none';
+        if (showDivider) {
+          divider.style.display = 'block';
+        }
+        else {
+          divider.style.display = 'none';
+        }
     }
 
     const loadModel = async () => {
         setIsModelLoading(true)
         try {
-            const model = await mobilenet.load({ version: 2,
-              alpha: 1.0 });
-            setModel(model);
-            setIsModelLoading(false);
+          // mobilenet is the module name, which is automatically included when you use the <script src> method
+          // When using ES6 imports, mobilenet is the module.
+          // Refer: https://github.com/tensorflow/tfjs-models/tree/master/mobilenet
+          const model = await mobilenet.load({ version: 2,
+            alpha: 1.0 });
+          setModel(model);
+          setIsModelLoading(false);
         } catch (error) {
-            console.log(error)
-            setIsModelLoading(false)
+          console.log(error)
+          setIsModelLoading(false)
         }
     }
 
@@ -63,18 +70,21 @@ function App() {
 
     // Predict the probable classes of the image
     const classifyImage = async () => {
-        const classificationResults = await model.classify(imageRef.current)
-        setResults(classificationResults)
-        showResultHolderDisplay(true);
-        showPageDivider(true);
+      // You can make a classification with mobilenet without needing to create a Tensor with
+      // MobileNet.classify, which takes an input image element and returns an array with top classes and their probabilities.
+      // Refer: https://github.com/tensorflow/tfjs-models/tree/master/mobilenet
+      const classificationResults = await model.classify(imageRef.current)
+      setResults(classificationResults)
+      showResultHolderDisplay(true);
+      showPageDivider(true);
     }
 
     const triggerUpload = () => {
-        fileInputRef.current.click()
+      fileInputRef.current.click()
     }
 
     useEffect(() => {
-        loadModel()
+      loadModel()
     }, [])
 
     if (isModelLoading) {
